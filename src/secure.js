f19996575d0dee34f46bb5673fa031f422725013
JigmeNidup <jigmenidup8102003@gmail.com>
2021-08-30 11:10:05 +0700

    deploy my react app to github pages

---
+++ src/secure.js
@@ -0,0 +1,19 @@
+
+
+function Secure(){
+
+function Shawer(str){
+
+    return str +"i know";
+
+}
+
+console.log(Shawer("kill "));
+return (
+        <div>
+        {console.log(Shawer("bill "))}
+        </div>
+    );
+}
+
+export default Secure();
